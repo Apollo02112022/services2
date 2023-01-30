1f7692e9be4623aade8a304ba3548c4cdedd29f8
Apollo02112022 <nicolas.fuente@sncf.fr>
2023-01-30 17:04:54 +0100

    third commit

---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Cocktail } from './cocktail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CocktailService {
+
+  constructor(public http: HttpClient) { } 
+
+  getCocktails(): Observable<Cocktail[]> {
+    return this.http.get<Cocktail[]>("assets/cocktails.json");
+  }
+
+}
---
+++ src/app/cocktail.ts
@@ -0,0 +1,7 @@
+export class Cocktail {
+    constructor(
+        public name: string,
+        public price: string,
+        public picture: string
+    ) {}
+}
